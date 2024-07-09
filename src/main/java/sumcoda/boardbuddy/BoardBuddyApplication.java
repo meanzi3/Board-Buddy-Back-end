c50eadae4ffe459ef29d60b73731c3f835ef6a0b
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-10 00:46:14 +0900

    Init: 프로젝트 초기 설정
    - .gitignore 추가
    - application.properties 추가
    - BoardBuddyApplication.java 추가
    - build.gradle 추가
    - gradle-wrapper.jar 추가
    - gradle-wrapper.properties 추가
    - gradlew 추가
    - gradlew.bat 추가
    - settings.gradle 추가

---
+++ src/main/java/sumcoda/boardbuddy/BoardBuddyApplication.java
@@ -0,0 +1,13 @@
+package sumcoda.boardbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBuddyApplication.class, args);
+    }
+
+}

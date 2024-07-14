2034e762d6b3754bb8e9f883946bd1a7e9384a6d
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-15 03:18:50 +0900

    Feat: SecurityConfig 구현
    - 사용자 요청에 대한 보안 설정을 편리하게 리하기 위한 SecurityConfig.java 구현
    - 사용자의 비밀번호 암호화를 위한 BCryptPasswordEncoder 인스턴스를 Bean으로 등록
    - SecurityFilterChain을 커스텀 하기위한 filterChain() 메서드 구현

---
+++ src/main/java/sumcoda/boardbuddy/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package sumcoda.boardbuddy.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/**", "/oauth2/authorization/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}

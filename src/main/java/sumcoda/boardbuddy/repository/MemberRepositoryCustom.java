e07449efdec4a37c3fb0eba22543c86829b1ddcf
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-15 13:16:17 +0900

    Feat: 로그인 구현을 위한 Member 관련 Repository 코드 추가
    - MemberRepositoryCustom.java
    - 로그인 로직에 필요한 사용자의 정보를 조회하기 위한 findAuthDTOByUsername() 메서드 선언
    - 로그인 성공시 프론트로 해당 사용자의 프로필 정보를 전달하기 위하여 해당 사용자의 프로필 정보를 조회하기 위한 findMemberDTOByUsername() 메서드 선언
    
    - MemberRepositoryCustomImpl.java
    - findAuthDTOByUsername() 메서드 구현
    - findMemberDTOByUsername() 메서드 구현

--- src/main/java/sumcoda/boardbuddy/repository/MemberRepositoryCustom.java
+++ src/main/java/sumcoda/boardbuddy/repository/MemberRepositoryCustom.java
@@ -1,4 +1,14 @@
 package sumcoda.boardbuddy.repository;
 
+import sumcoda.boardbuddy.dto.AuthResponse;
+import sumcoda.boardbuddy.dto.MemberResponse;
+
+import java.util.Optional;
+
 public interface MemberRepositoryCustom {
+
+    Optional<AuthResponse.ProfileDTO> findAuthDTOByUsername(String username);
+
+    Optional<MemberResponse.ProfileDTO> findMemberDTOByUsername(String username);
+
 }

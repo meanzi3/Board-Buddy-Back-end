74c54097dbd2c3a0ceab7301de8ec21bf08ed0f9
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-15 13:01:52 +0900

    Feat: 로그인 구현을 위한 Config 파일 작성
    - QueryDSL 활용을 위한 QueryDSLConfig.java 작성
    - JPAQueryFactory 반환을 위한 jpaQueryFactory() 메서드 Bean으로 등록
    
    - 로그인 설정 및 cors 설정을 위한 SecurityConfig.java 내부에 코드 추가
    - 필드: authenticationConfiguration, customAuthenticationSuccessHandler, customAuthenticationFailureHandler, customAuthenticationEntryPoint, customAccessDeniedHandler -> 로그인 구현에 필요한 필드 추가
    - cors 설정을 위한 corsConfigurationSource() 메서드 Bean으로 등록
    - 보안을 위한 HTTP 응답 헤더 설정 추가 작성
    - 효율적인 세션 관리를 위한 코드 추가 작성
    - 로그인 구현에 필요한 컴포넌트를 반환하기 위한 customAuthenticationFilter() 메서드를 Bean으로 등록
    - 로그인 구현에 필요한 AuthenticationManager를 반환하기 위한 authenticationManager() 메서드를 Bean으로 등록

--- src/main/java/sumcoda/boardbuddy/config/SecurityConfig.java
+++ src/main/java/sumcoda/boardbuddy/config/SecurityConfig.java
@@ -3,32 +3,169 @@ package sumcoda.boardbuddy.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import sumcoda.boardbuddy.component.auth.CustomAccessDeniedHandler;
+import sumcoda.boardbuddy.component.auth.CustomAuthenticationEntryPoint;
+import sumcoda.boardbuddy.handler.auth.CustomAuthenticationFailureHandler;
+import sumcoda.boardbuddy.filter.CustomAuthenticationFilter;
+import sumcoda.boardbuddy.handler.auth.CustomAuthenticationSuccessHandler;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 허용할 출처 설정
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        // 허용할 HTTP 메서드 설정
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("OPTIONS");
+        configuration.addAllowedMethod("DELETE");
+        configuration.addAllowedMethod("PUT");
+
+        // 허용할 헤더 설정
+        configuration.addAllowedHeader("Origin");
+        configuration.addAllowedHeader("Content-Type");
+        configuration.addAllowedHeader("Accept");
+        configuration.addAllowedHeader("Cache-Control");
+        configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("X-AUTH-TOKEN");
+        configuration.addAllowedHeader("Authorization_Refresh");
+        configuration.addAllowedHeader("Access-Control-Allow-Origin");
+        configuration.addAllowedHeader("Access-Control-Allow-Credentials");
+
+        // 노출할 헤더 설정
+        configuration.addExposedHeader("Content-Type");
+        configuration.addExposedHeader("X-AUTH-TOKEN");
+        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("Authorization_Refresh");
+        configuration.addExposedHeader("Access-Control-Allow-Origin");
+        configuration.addExposedHeader("Access-Control-Allow-Credentials");
+
+        // 자격 증명 허용 설정
+        configuration.setAllowCredentials(true);
+
+        // pre-flight 요청 캐싱 시간 설정
+        configuration.setMaxAge(1728000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 새로추가
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource())))
+                // 보안 컨텍스트의 저장 방식을 제어하는 설정
+                // 보안 컨텍스트가 명시적으로 저장될 때만 저장되도록 한다.
+                // 보안 컨텍스트가 실수로 변경되거나 저장되는 것을 방지하여 보안성을 높인다.
+                .securityContext(securityContext -> securityContext
+                        .requireExplicitSave(true))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/**", "/oauth2/authorization/**").permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated())
+
+                // 추가 코드
+                .addFilterAt(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(config -> config
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler))
+                // HTTP 응답 헤더를 설정한다.
+                // frameOptions 설정을 sameOrigin 으로 설정하여,
+                // 현재 페이지가 동일한 출처의 페이지에서만 포함될 수 있도록 한다.
+                .headers(
+                        headersConfigurer -> headersConfigurer
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                )
+                );
+
+        http    // 하나의 아이디에 대해서 다중 로그인에 대한 처리
+                .sessionManagement(auth -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        http
+                .sessionManagement(auth -> auth
+                        .sessionFixation()
+                        .changeSessionId());
+
+        http
+                .sessionManagement(auth -> auth
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
+                .rememberMe(auth -> auth
+//                        .useSecureCookie(true) // HTTPS 환경에서만 쿠키전송
+                        .tokenValiditySeconds(86400));
 
         return http.build();
+    }
+
+    // 새로추가
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customAuthenticationFilter.setAuthenticationSuccessHandler(customAuthenticationSuccessHandler);
+        customAuthenticationFilter.setAuthenticationFailureHandler(customAuthenticationFailureHandler);
+
+        customAuthenticationFilter.setSecurityContextRepository(
+                new DelegatingSecurityContextRepository(
+                        new HttpSessionSecurityContextRepository(),
+                        new RequestAttributeSecurityContextRepository()
+                ));
+
+        return customAuthenticationFilter;
+    }
+
+    // AuthenticationManager 를 반환하는 메서드 등록
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }

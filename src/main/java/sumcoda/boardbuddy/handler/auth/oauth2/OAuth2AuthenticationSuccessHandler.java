6dc4765e2afa33ab3783ee867f4b9c0f1eba3bd1
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-16 18:38:26 +0900

    Feat: 소셜 로그인을 구현하기위한 핸들러 수정
    - OAuth2AuthenticationSuccessHandler.java
    - 핸드폰 번호 데이터의 null 여부로 해당 사용자가 첫 로그인 사용자인지 판별하여 첫 로그인 사용자와 그렇지 않은 사용자를 구분하여 클라이언트에게 리다이렉트
    - 리다이렉트시 클라이언트에게 전달하는 파라미터 추가 및 리다이렉트 도메인 변경
    
    - OAuth2AuthenticationFailureHandler.java
    - 리다이렉트시 클라이언트에게 전달하는 파라미터 추가 리다이렉트 도메인 변경

--- src/main/java/sumcoda/boardbuddy/handler/auth/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/sumcoda/boardbuddy/handler/auth/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -8,11 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import sumcoda.boardbuddy.dto.auth.oauth2.CustomOAuth2User;
+import sumcoda.boardbuddy.entity.Member;
+import sumcoda.boardbuddy.repository.MemberRepository;
 
 import java.io.IOException;
 
@@ -21,6 +24,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final MemberRepository memberRepository;
+    
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
@@ -28,12 +33,30 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
         log.info("OAuth2 success handler is working");
         CustomOAuth2User user =  (CustomOAuth2User) authentication.getPrincipal();
-        log.info("login user name : " + user.getUsername());
+        Boolean isPhoneNumberVerifiedMember = checkIsPhoneNumberVerifiedMember(user);
+
 
         response.setStatus(HttpStatus.OK.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
 
-        redirectStrategy.sendRedirect(request, response , "https://boardbuddyapp.vercel.app/login/oauth/callback?isSuccess=true");
+        // 최초 로그인 사용자가 아닌 사용자인 경우
+        if (Boolean.TRUE.equals(isPhoneNumberVerifiedMember)) {
+            redirectStrategy.sendRedirect(request, response, "https://boardbuddyapp.vercel.app/login/oauth/callback?isLoginSucceed=true&isVerifiedMember=true&message=로그인에 성공하였습니다.");
+        // 최초 로그인 사용자인 경우
+        } else {
+            redirectStrategy.sendRedirect(request, response , "https://boardbuddyapp.vercel.app/login/oauth/callback?isLoginSucceed=true&isVerifiedMember=false&message=로그인에 성공하였습니다.");
+        }
     }
+
+    private Boolean checkIsPhoneNumberVerifiedMember(CustomOAuth2User user) {
+        String username = user.getUsername();
+
+        log.info("login user name : " + username);
+
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(""));
+
+        return member.getPhoneNumber() != null;
+    }
+
 }

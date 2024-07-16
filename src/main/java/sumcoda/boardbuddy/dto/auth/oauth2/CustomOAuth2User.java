7a5bd4f90008cf48faf927486563ea4ae5431d80
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-16 11:20:42 +0900

    Feat: 소셜 로그인 시도시 DB에서 조회후 유저 정보를 담을 dto와 로직, DB 조회 메서드 구현
    - MemberRepository.java
    - findByUsername() -> 유저이름과 일치하는 Optional<Member> 반환하는 메서드 선언
    
    - CustomOAuth2UserService.java
    - 소셜 로그인 시도시 DB에서 로그인을 시도하는 유저의 정보를 조회하기 위한 로직 구현
    
    - CustomOAuth2User.java
    - 소셜 로그인을 시도하는 유저의 정보를 DB에서 조회한후 해당 유저의 정보를 담아서 AuthticationManger에게 전달하기 위한 DTO 구현

---
+++ src/main/java/sumcoda/boardbuddy/dto/auth/oauth2/CustomOAuth2User.java
@@ -0,0 +1,40 @@
+package sumcoda.boardbuddy.dto.auth.oauth2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import sumcoda.boardbuddy.enumerate.MemberRole;
+
+import java.util.*;
+
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+    private final OAuth2UserInfo oAuth2UserInfo;
+
+    private final MemberRole role;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add((GrantedAuthority) role::getValue);
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2UserInfo.getName();
+    }
+
+    public String getUsername() {
+        return oAuth2UserInfo.getProvider() + " " + oAuth2UserInfo.getProviderId();
+    }
+}

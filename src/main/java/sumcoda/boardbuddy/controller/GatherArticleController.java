e889844ec680bcd8fd12dd16d3920e3cfeb1ba6c
meanzi3 <minjichoe0326@gmail.com>
2024-07-17 14:25:57 +0900

    Feat: 모집글 CRUD를 위한 Controller 작성
    - 모집글 작성, 조회, 수정, 삭제 컨트롤러를 작성함.

--- src/main/java/sumcoda/boardbuddy/controller/GatherArticleController.java
+++ src/main/java/sumcoda/boardbuddy/controller/GatherArticleController.java
@@ -6,9 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sumcoda.boardbuddy.dto.GatherArticleRequest;
 import sumcoda.boardbuddy.dto.GatherArticleResponse;
+import sumcoda.boardbuddy.dto.common.ApiResponse;
 import sumcoda.boardbuddy.service.GatherArticleService;
 
 import java.util.HashMap;
@@ -43,6 +44,68 @@ public class GatherArticleController {
         response.put("message", "내 모집글이 성공적으로 조회되었습니다.");
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    /**
+     * 모집글 작성 컨트롤러
+     *
+     * @param createRequest
+     * @param username
+     * @return
+     */
+    @PostMapping(value = "/api/gatherArticles")
+    public ResponseEntity<ApiResponse<GatherArticleResponse.CreateDTO>> createGatherArticle(
+            @RequestBody GatherArticleRequest.CreateDTO createRequest,
+            @ModelAttribute("username") String username){
+        GatherArticleResponse.CreateDTO createResponse = gatherArticleService.createGatherArticle(createRequest, username);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(createResponse, "모집글이 업로드 되었습니다"));
+    }
+
+    /**
+     * 모집글 조회 컨트롤러
+     *
+     * @param gatherArticleId
+     * @param username
+     * @return
+     */
+    @GetMapping(value = "/api/gatherArticles/{gatherArticleId}")
+    public ResponseEntity<ApiResponse<GatherArticleResponse.ReadDTO>> getGatherArticle(
+            @PathVariable Long gatherArticleId,
+            @ModelAttribute("username") String username) {
+        GatherArticleResponse.ReadDTO response = gatherArticleService.getGatherArticle(gatherArticleId, username);
+        return ResponseEntity.ok(new ApiResponse<>(response, "성공적으로 조회되었습니다."));
+    }
+
+    /**
+     * 모집글 수정 컨트롤러
+     *
+     * @param gatherArticleId
+     * @param updateRequest
+     * @param username
+     * @return
+     */
+    @PutMapping(value = "/api/gatherArticles/{gatherArticleId}")
+    public ResponseEntity<ApiResponse<GatherArticleResponse.UpdateDTO>> updateGatherArticle(
+            @PathVariable Long gatherArticleId,
+            @RequestBody GatherArticleRequest.UpdateDTO updateRequest,
+            @ModelAttribute("username") String username){
+        GatherArticleResponse.UpdateDTO updateResponse = gatherArticleService.updateGatherArticle(gatherArticleId, updateRequest, username);
+        return ResponseEntity.ok(new ApiResponse<>(updateResponse, "모집글이 수정되었습니다."));
+    }
+
+    /**
+     * 모집글 삭제 컨트롤러
+     * 
+     * @param gatherArticleId
+     * @param username
+     * @return
+     */
+    @DeleteMapping(value = "/api/gatherArticles/{gatherArticleId}")
+    public ResponseEntity<ApiResponse<GatherArticleResponse.DeleteDTO>> deleteGatherArticle(
+            @PathVariable Long gatherArticleId,
+            @ModelAttribute("username") String username){
+        GatherArticleResponse.DeleteDTO deleteResponse = gatherArticleService.deleteGatherArticle(gatherArticleId, username);
+        return ResponseEntity.ok(new ApiResponse<>(deleteResponse, "모집글이 삭제되었습니다."));
     }
 }
 

14b9b1ab184fe45c436c53738477fedbfbe87335
geunhokinn <gghh1220@gmail.com>
2024-07-25 00:36:42 +0900

    Feat: GatherArticleSortException, GatherArticleStatusException, NearPublicDistrictRetrievalException, PublicDistrictRetrievalException 작성
    - GatherArticleSortException.java
    - 모집글 리스트 조회에서 정렬 기준에 대한 예외를 처리하기 위한 GatherArticleSortException 작성
    
    - GatherArticleStatusException.java
    - 모집글 리스트 조회에서 상태 조건에 대한 예외를 처리하기 위한 GatherArticleStatusException 작성
    
    - NearPublicDistrictRetrievalException.java
    - 서버 문제로 주변 행정 구역 조회가 되지 않는 예외를 처리하기 위한 NearPublicDistrictRetrievalException 작성
    
    - PublicDistrictRetrievalException.java
    - 서버 문제로 행정 구역 조회가 되지 않는 예외를 처리하기 위한 PublicDistrictRetrievalException 작성(기존의 PublicDistrictNotFoundException 예외 삭제)

---
+++ src/main/java/sumcoda/boardbuddy/exception/nearPublicDistrict/NearPublicDistrictRetrievalException.java
@@ -0,0 +1,8 @@
+package sumcoda.boardbuddy.exception.nearPublicDistrict;
+
+public class NearPublicDistrictRetrievalException extends RuntimeException {
+
+    public NearPublicDistrictRetrievalException(String message) {
+        super(message);
+    }
+}

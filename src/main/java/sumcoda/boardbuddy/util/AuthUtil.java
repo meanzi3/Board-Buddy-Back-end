54fd6a748cdc6039ed88935dc9878db711d5be3d
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-25 18:36:28 +0900

    Fix: 배포 환경 이슈 해결을 위한 수정
    - AuthUtil.java
    - getUserNameByLoginType() 메서드 로직 수정

--- src/main/java/sumcoda/boardbuddy/util/AuthUtil.java
+++ src/main/java/sumcoda/boardbuddy/util/AuthUtil.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import sumcoda.boardbuddy.dto.auth.oauth2.CustomOAuth2User;
 import sumcoda.boardbuddy.exception.auth.AuthenticationMissingException;
@@ -25,28 +24,19 @@ public class AuthUtil {
             throw new AuthenticationMissingException("유효하지 않은 사용자의 요청입니다.(인터셉터 동작)");
         }
 
+        String username;
 
-        Object principal = authentication.getPrincipal();
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            // OAuth2.0 사용자
+            CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+            username = oauthUser.getUsername();
 
-        if (principal instanceof UserDetails userDetails) {
-            return userDetails.getUsername();
+            // 그외 사용자
+        } else {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
         }
-        else if (principal instanceof OAuth2User) {
-            CustomOAuth2User oAuth2User = (CustomOAuth2User) principal;
-            return oAuth2User.getUsername();
-        }
-//        // OAuth2.0 사용자
-//        if (authentication instanceof OAuth2AuthenticationToken) {
-////            CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
-////            username = oauthUser.getUsername();
-//            username = authentication.getPrincipal().toString();
-//
-//            // 그외 사용자
-//        } else {
-////            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-////            username = userDetails.getUsername();
-//            username = authentication.getPrincipal().toString();
-//        }
-        return null;
+
+        return username;
     }
 }

81988d290decc7d090049e8181088d5528a56d8e
geunhokinn <gghh1220@gmail.com>
2024-07-25 14:24:47 +0900

    Refactor: BoardCafeResponse 수정 - BoardCafeResponse.java - 보드 게임 카페 찾기 응답 DTO 의 불필요한 어노테이션 제거

--- src/main/java/sumcoda/boardbuddy/dto/BoardCafeResponse.java
+++ src/main/java/sumcoda/boardbuddy/dto/BoardCafeResponse.java
@@ -1,6 +1,5 @@
 package sumcoda.boardbuddy.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,31 +10,22 @@ public class BoardCafeResponse {
     @NoArgsConstructor
     public static class InfoDTO {
 
-        @JsonProperty("address_name")
         private String addressName;
 
-        @JsonProperty("distance")
         private Integer distance;
 
-        @JsonProperty("id")
         private Integer id;
 
-        @JsonProperty("phone")
         private String phone;
 
-        @JsonProperty("place_name")
         private String placeName;
 
-        @JsonProperty("place_url")
         private String placeUrl;
 
-        @JsonProperty("road_address_name")
         private String roadAddressName;
 
-        @JsonProperty("x")
         private Double x;
 
-        @JsonProperty("y")
         private Double y;
 
         @Builder

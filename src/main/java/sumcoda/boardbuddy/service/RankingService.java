93a3ea9adfe7f8a45f178a6f5408727c590f432c
meanzi3 <minjichoe0326@gmail.com>
2024-07-25 14:57:08 +0900

    Refactor: RankingService 리팩토링
    - 랭킹 조회 메서드에 @Transactional(readOnly = true) 추가
    - 기존: for문을 이용한 반복 -> 수정: stream 이용
    - TOP_RANK_COUNT 상수 이용 (3)
    - 지난달 YearMonth를 구하는 메서드 추가 작성

--- src/main/java/sumcoda/boardbuddy/service/RankingService.java
+++ src/main/java/sumcoda/boardbuddy/service/RankingService.java
@@ -15,9 +15,12 @@ import sumcoda.boardbuddy.repository.gatherArticle.GatherArticleRepository;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @Slf4j
@@ -33,12 +36,16 @@ public class RankingService {
 
     private final MemberJdbcRepository memberJdbcRepository;
 
+    private final BadgeImageService badgeImageService;
+
+    private static final int TOP_RANK_COUNT = 3;
+
 
     /**
      * 랭킹 조회
      * @return TOP3 RankingsDTO list
      */
-    @Transactional
+    @Transactional(readOnly = true)
     public List<MemberResponse.RankingsDTO> getTop3Rankings(){
 
         return memberRepository.findTop3RankingMembers();
@@ -58,18 +65,20 @@ public class RankingService {
         LocalDateTime endOfLastMonth = getEndOfLastMonth();
 
         List<Member> members = memberRepository.findAll();
-        Map<Long, Double> memberScores = new HashMap<>();
 
         // 점수 계산
-        for (Member member : members) {
-            // 지난 달 모집글 갯수
-            long gatherArticleCount = gatherArticleRepository.countGatherArticlesByMember(member, startOfLastMonth, endOfLastMonth);
-            // 지난 달 댓글 갯수
-            long commentCount = commentRepository.countCommentsByMember(member, startOfLastMonth, endOfLastMonth);
-            // 후기 카운트, 리뷰 보낸 횟수 합하여 점수 계산
-            double rankScore = calculateRankScore(member, gatherArticleCount, commentCount);
-            memberScores.put(member.getId(), rankScore);
-        }
+        Map<Long, Double> memberScores = members.stream()
+                .collect(Collectors.toMap(
+                        Member::getId,
+                        member -> {
+                            // 지난 달 모집글 갯수
+                            long gatherArticleCount = gatherArticleRepository.countGatherArticlesByMember(member, startOfLastMonth, endOfLastMonth);
+                            // 지난 달 댓글 갯수
+                            long commentCount = commentRepository.countCommentsByMember(member, startOfLastMonth, endOfLastMonth);
+                            // 후기 카운트, 리뷰 보낸 횟수 합하여 점수 계산
+                            return calculateRankScore(member, gatherArticleCount, commentCount);
+                        }
+                ));
 
         // 점수 업데이트
         memberJdbcRepository.updateMemberRankScores(memberScores);
@@ -77,22 +86,14 @@ public class RankingService {
         // 점수 별로 정렬
         List<Member> orderedByScoreMembers = memberRepository.findAllOrderedByRankScore();
 
-        // 랭킹 업데이트
+        // 각 member 들의 랭킹 구하기
         Map<Long, Integer> rankUpdateMap = new HashMap<>();
-
-        for (int i = 0; i < orderedByScoreMembers.size(); i++) {
-            // TODO : 1 ~ 3등 뱃지 부여
-            Member member = orderedByScoreMembers.get(i);
-            if (i == 0) {
-              rankUpdateMap.put(member.getId(), 1);
-            } else if (i == 1) {
-              rankUpdateMap.put(member.getId(), 2);
-            } else if (i == 2) {
-              rankUpdateMap.put(member.getId(), 3);
-            } else {
-              rankUpdateMap.put(member.getId(), null);
-            }
-        }
+        IntStream.range(0, orderedByScoreMembers.size())
+                .forEach(i -> {
+                    Long memberId = orderedByScoreMembers.get(i).getId();
+                    Integer rank = (i < TOP_RANK_COUNT) ? i + 1 : null;
+                    rankUpdateMap.put(memberId, rank);
+                });
 
         // 랭킹 업데이트
         memberJdbcRepository.updateMemberRanks(rankUpdateMap);
@@ -100,18 +101,32 @@ public class RankingService {
         // 후기 카운트, 보낸 리뷰 카운트 초기화
         memberJdbcRepository.resetMonthlyCounts();
 
+        // TOP3 member들의 id 담기
+        List<Long> top3MemberIds = new ArrayList<>();
+
+        for (int i = 0; i < Math.min(orderedByScoreMembers.size(), TOP_RANK_COUNT); i++) {
+            top3MemberIds.add(orderedByScoreMembers.get(i).getId());
+        }
+
+        // TOP3에게 뱃지 부여
+        badgeImageService.assignBadgesToTopMembersByIds(top3MemberIds, getLastMonth());
     }
 
     // 지난 달 시작일
     private LocalDateTime getStartOfLastMonth() {
-        YearMonth lastMonth = YearMonth.now().minusMonths(1);
+        YearMonth lastMonth = getLastMonth();
         return lastMonth.atDay(1).atStartOfDay();
     }
 
     // 지난 달 종료일
     private LocalDateTime getEndOfLastMonth() {
-        YearMonth lastMonth = YearMonth.now().minusMonths(1);
+        YearMonth lastMonth = getLastMonth();
         return lastMonth.atEndOfMonth().atTime(23, 59, 59);
+    }
+
+    // 지난달 YearMonth 구하기
+    private YearMonth getLastMonth() {
+        return YearMonth.now().minusMonths(1);
     }
 
     // 점수 계산

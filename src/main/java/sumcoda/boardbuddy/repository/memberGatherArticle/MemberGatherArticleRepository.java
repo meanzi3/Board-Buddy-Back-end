14f2bc93181970d3bf36837dd872eaf0ae21538a
geunhokinn <gghh1220@gmail.com>
2024-07-27 18:02:35 +0900

    Feat: MemberGatherArticleRepository
    - MemberGatherArticleRepository.java
    - @Repository 추가

--- src/main/java/sumcoda/boardbuddy/repository/memberGatherArticle/MemberGatherArticleRepository.java
+++ src/main/java/sumcoda/boardbuddy/repository/memberGatherArticle/MemberGatherArticleRepository.java
@@ -1,7 +1,9 @@
 package sumcoda.boardbuddy.repository.memberGatherArticle;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sumcoda.boardbuddy.entity.MemberGatherArticle;
 
+@Repository
 public interface MemberGatherArticleRepository extends JpaRepository<MemberGatherArticle, Long>, MemberGatherArticleRepositoryCustom {
 }

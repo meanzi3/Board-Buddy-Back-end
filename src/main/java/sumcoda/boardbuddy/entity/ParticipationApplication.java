8c2b693d77ef7de787970854dce885dca13cc533
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-28 09:06:00 +0900

    Feat: 참가신청 상태 관리를 위한 ParticipationApplication 구현
    - ParticipationApplication.java
    - 필드: id, rejectedParticipationCount, participationApplicationStatus, memberGatherArticle
    - 연관관계: MemberGatherArticle 엔티티와 1 <-> 1 양방향 연관관계 설정 ParticipationApplication(연관관계 주인)1 <-> 1MemberGatherArticle
    - 생성자: ParticipationApplication() -> 빌더 패턴 생성자
    - buildParticipationRequest() -> 직접 빌더 패턴의 생성자를 활용하지 않고 해당 메서드를 활용하여 엔티티 생성
    - assignMemberGatherArticle() -> MemberGatherArticle 엔티티와 양방향 연관관계를 설정하기위한 양방향 연관관계 편의 메서드 구현
    - assignParticipationApplicationStatus() -> 모집글 참가신청에 대한 사용자의 상태
    - assignRejectedParticipationCount() -> 동일한 모집글에 대해 참가 거절 당한 횟수 업데이트
    
    - MemberGatherArticleRole.java
    - 열거형 상수 필드: NONE, PENDING, APPROVED,  REJECTED, CANCELED
    - 필드: value

---
+++ src/main/java/sumcoda/boardbuddy/entity/ParticipationApplication.java
@@ -0,0 +1,78 @@
+package sumcoda.boardbuddy.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sumcoda.boardbuddy.enumerate.ParticipationApplicationStatus;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ParticipationApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 참가 신청 거절 응답 횟수
+    @Column(nullable = false)
+    private Integer rejectedParticipationCount;
+
+    // 모집글에 참가 신청하는 사용자의 상태를 나타내기위한 role
+    // ex) NONE, PENDING, APPROVED, CANCELED
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ParticipationApplicationStatus participationApplicationStatus;
+
+    // 연관관계 주인
+    // 양방향 관계
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_gather_article_id")
+    private MemberGatherArticle memberGatherArticle;
+
+    @Builder
+    public ParticipationApplication(Integer rejectedParticipationCount, ParticipationApplicationStatus participationApplicationStatus, MemberGatherArticle memberGatherArticle) {
+        this.rejectedParticipationCount = rejectedParticipationCount;
+        this.participationApplicationStatus = participationApplicationStatus;
+        this.assignMemberGatherArticle(memberGatherArticle);
+
+    }
+
+    // 직접 빌더 패턴의 생성자를 활용하지 말고 해당 메서드를 활용하여 엔티티 생성
+    public static ParticipationApplication buildParticipationRequest(Integer rejectedParticipationCount, ParticipationApplicationStatus participationApplicationStatus, MemberGatherArticle memberGatherArticle) {
+        return ParticipationApplication.builder()
+                .rejectedParticipationCount(rejectedParticipationCount)
+                .participationApplicationStatus(participationApplicationStatus)
+                .memberGatherArticle(memberGatherArticle)
+                .build();
+    }
+
+    // ParticipationApplication 1 <-> 1 MemberGatherArticle
+    // 양방향 연관관계 편의 메서드
+    public void assignMemberGatherArticle(MemberGatherArticle memberGatherArticle) {
+        if (this.memberGatherArticle != null) {
+            this.memberGatherArticle.assignParticipationApplication(null);
+        }
+        this.memberGatherArticle = memberGatherArticle;
+        if (memberGatherArticle != null && memberGatherArticle.getParticipationApplication() != this) {
+            memberGatherArticle.assignParticipationApplication(this);
+        }
+    }
+
+
+
+    // 모집글 참가신청에 대한 사용자의 상태 업데이트
+    public void assignParticipationApplicationStatus(ParticipationApplicationStatus participationApplicationStatus) {
+        this.participationApplicationStatus = participationApplicationStatus;
+    }
+
+    // 동일한 모집글에 대해 참가 거절 당한 횟수 업데이트
+    public void assignRejectedParticipationCount(Integer rejectedParticipationCount) {
+        this.rejectedParticipationCount = rejectedParticipationCount;
+    }
+
+
+}
+
---
+++ src/main/java/sumcoda/boardbuddy/enumerate/ParticipationApplicationStatus.java
@@ -0,0 +1,17 @@
+package sumcoda.boardbuddy.enumerate;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ParticipationApplicationStatus {
+
+    NONE("none"), // 참여 없음 상태
+    PENDING("pending"),  // 참여 대기 상태
+    APPROVED("approved"), // 참여 승인 상태
+    REJECTED("rejected"), // 참여 거절 상태
+    CANCELED("canceled");  // 참여 취소 상태
+
+    private final String value;
+}

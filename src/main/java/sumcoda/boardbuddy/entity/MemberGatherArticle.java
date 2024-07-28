5a1fc67f2944884156f6d7092bad2a8858ce07cf
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-28 09:14:01 +0900

    Refactor: 참가신청 로직 구현을 위한 MemberGatherArticle 엔티티 리팩토링
    - MemberGatherArticle.java
    - 필드: isPermit -> 삭제, GatherArticleRole -> MemberGatherArticleRole 이름 수정, participationApplication -> 추가
    - 연관관계 설정: ParticipationApplication 엔티티와 1 <-> 1 양방향 연관관계 설정 1MemberGatherArticle1 <-> ParticipationApplication(연관관계 주인)
    - MemberGatherArticleRole 이름 수정으로 인한 생성자 및
    buildMemberGatherArticle() 수정
    - assignParticipationApplication() -> ParticipationApplication 엔티티와 양방향 연관관계를 설정하기위한 양방향 연관관계 편의 메서드 구현
    - assignJoinedAt() -> 사용자가 모집글에 참여된 시점 업데이트
    - assignMemberGatherArticleRole() -> 모집글에 참여한 사용자의 권한 업데이트
    
    - GatherArticleRole.java
    - GatherArticleRole -> MemberGatherArticleRole 이름 수정
    
    - MemberGatherArticleRole.java
    - 열거형 상수 필드: NONE, AUTHOR, PARTICIPANT
    - 필드: value

--- src/main/java/sumcoda/boardbuddy/entity/MemberGatherArticle.java
+++ src/main/java/sumcoda/boardbuddy/entity/MemberGatherArticle.java
@@ -5,7 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import sumcoda.boardbuddy.enumerate.GatherArticleRole;
+import sumcoda.boardbuddy.enumerate.MemberGatherArticleRole;
 
 import java.time.LocalDateTime;
 
@@ -21,15 +21,11 @@ public class MemberGatherArticle {
     // 해당 유저가 방에 참석한 시점
     private LocalDateTime joinedAt;
 
-    // 모집글 참여자의 role
-    @Column(nullable = false)
-    private Boolean isPermit;
-
     // 모집글 참여자의 권한을 나타내기위한 role
     // ex) AUTHOR, PARTICIPANT
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private GatherArticleRole gatherArticleRole;
+    private MemberGatherArticleRole memberGatherArticleRole;
 
     // 양방향 연관관계
     // 연관관계 주인
@@ -43,21 +39,23 @@ public class MemberGatherArticle {
     @JoinColumn(name = "gather_article_id")
     private GatherArticle gatherArticle;
 
+    // 양방향 연관관계
+    @OneToOne(mappedBy = "memberGatherArticle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ParticipationApplication participationApplication;
+
     @Builder
-    public MemberGatherArticle(LocalDateTime joinedAt, Boolean isPermit, GatherArticleRole gatherArticleRole, Member member, GatherArticle gatherArticle) {
+    public MemberGatherArticle(LocalDateTime joinedAt, MemberGatherArticleRole memberGatherArticleRole, Member member, GatherArticle gatherArticle) {
         this.joinedAt = joinedAt;
-        this.isPermit = isPermit;
-        this.gatherArticleRole = gatherArticleRole;
+        this.memberGatherArticleRole = memberGatherArticleRole;
         this.assignMember(member);
         this.assignGatherArticle(gatherArticle);
     }
 
     // 직접 빌더 패턴의 생성자를 활용하지 말고 해당 메서드를 활용하여 엔티티 생성
-    public static MemberGatherArticle buildMemberGatherArticle(LocalDateTime joinedAt, Boolean isPermit, GatherArticleRole gatherArticleRole, Member member, GatherArticle gatherArticle) {
+    public static MemberGatherArticle buildMemberGatherArticle(LocalDateTime joinedAt, MemberGatherArticleRole memberGatherArticleRole, Member member, GatherArticle gatherArticle) {
         return MemberGatherArticle.builder()
                 .joinedAt(joinedAt)
-                .isPermit(isPermit)
-                .gatherArticleRole(gatherArticleRole)
+                .memberGatherArticleRole(memberGatherArticleRole)
                 .member(member)
                 .gatherArticle(gatherArticle)
                 .build();
@@ -87,6 +85,28 @@ public class MemberGatherArticle {
         if (!gatherArticle.getMemberGatherArticles().contains(this)) {
             gatherArticle.addMemberGatherArticle(this);
         }
+    }
+
+    // MemberGatherArticle 1 <-> 1 ParticipationApplication
+    // 양방향 연관관계 편의 메서드
+    public void assignParticipationApplication(ParticipationApplication participationApplication) {
+        if (this.participationApplication != null) {
+            this.participationApplication.assignMemberGatherArticle(null);
+        }
+        this.participationApplication = participationApplication;
+        if (participationApplication != null && participationApplication.getMemberGatherArticle() != this) {
+            participationApplication.assignMemberGatherArticle(this);
+        }
+    }
+
+    // 사용자가 모집글에 참여된 시점 업데이트
+    public void assignJoinedAt(LocalDateTime joinedAt) {
+        this.joinedAt = joinedAt;
+    }
+
+    // 모집글 관련 사용자의 권한 업데이트
+    public void assignMemberGatherArticleRole(MemberGatherArticleRole memberGatherArticleRole) {
+        this.memberGatherArticleRole = memberGatherArticleRole;
     }
 
 

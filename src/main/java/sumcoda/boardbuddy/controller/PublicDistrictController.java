16b885dac86c8bc693c8ddf71a5740fdfbb7b04e
geunhokinn <gghh1220@gmail.com>
2024-07-30 14:43:51 +0900

    Feat: PublicDistrictController, PublicDistrictService 작성
    - PublicDistrictController.java
    - 위치 검색 요청을 캐치하기 위한 searchLocations() 메서드 작성
    
    - PublicDistrictService.java
    - PublicDistrictController 에서 검색 요청 정보를 바탕으로 위치 검색 로직을 처리하기 위한 searchLocations() 메서드 작성

---
+++ src/main/java/sumcoda/boardbuddy/controller/PublicDistrictController.java
@@ -0,0 +1,40 @@
+package sumcoda.boardbuddy.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sumcoda.boardbuddy.dto.PublicDistrictResponse;
+import sumcoda.boardbuddy.dto.common.ApiResponse;
+import sumcoda.boardbuddy.service.PublicDistrictService;
+
+import java.util.List;
+import java.util.Map;
+
+import static sumcoda.boardbuddy.builder.ResponseBuilder.buildSuccessResponseWithPairKeyData;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class PublicDistrictController {
+
+    private final PublicDistrictService publicDistrictService;
+
+    /**
+     * 위치 검색 요청 처리
+     *
+     * @param emd 읍면동 검색어
+     * @return 검색된 위치 정보 리스트
+     */
+    @GetMapping("/api/locations/search")
+    public ResponseEntity<ApiResponse<Map<String, List<PublicDistrictResponse.InfoDTO>>>> searchLocations(
+            @RequestParam String emd,
+            @RequestAttribute String username) {
+        log.info("searchLocations is working");
+
+        List<PublicDistrictResponse.InfoDTO> locations = publicDistrictService.searchLocations(emd, username);
+
+        return buildSuccessResponseWithPairKeyData("locations", locations, "위치 검색을 성공하였습니다.", HttpStatus.OK);
+    }
+}

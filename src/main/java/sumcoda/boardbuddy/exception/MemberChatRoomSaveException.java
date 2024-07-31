84dafca22ae2e65673a43e2dcf4bcf3364b8da94
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-31 16:43:10 +0900

    Feat: 채팅방에 입장한 사용자 관련 정보 관리시 발생할 수 있는 예외 클래스 및 예외 핸들러 구현
    - MemberChatRoomNotFoundException.java
    - 찾을 수 없는, 채팅방에 입장한 사용자의 정보를 요청한 경우
    
    - MemberChatRoomRetrievalException.java
    - 서버 문제로 채팅방에 입장한 사용자의 정보를 찾을 수 없는 경우
    
    - MemberChatRoomSaveException.java
    - 서버 문제로 채팅방에 입장한 사용자의 정보를 저장하지 못한 경우
    
    - MemberChatRoomExceptionHandler.java
    - handleMemberChatRoomNotFoundException() 메서드: MemberChatRoomNotFoundException 등록
    - handleMemberChatRoomRetrievalException() 메서드: MemberChatRoomRetrievalException 등록
    - handleMemberChatRoomSaveException() 메서드: MemberChatRoomSaveException 등록

---
+++ src/main/java/sumcoda/boardbuddy/exception/MemberChatRoomSaveException.java
@@ -0,0 +1,7 @@
+package sumcoda.boardbuddy.exception;
+
+public class MemberChatRoomSaveException extends RuntimeException {
+    public MemberChatRoomSaveException(String message) {
+        super(message);
+    }
+}

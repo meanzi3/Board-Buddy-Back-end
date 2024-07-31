ee55eaeb8b94212bec563a8efa5fddf1b6af71a7
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-31 15:11:40 +0900

    Feat: 채팅방에 참여한 사용자 정보 관리 로직 구현을 위한 레포지토리 및 DTO 구현
    - MemberChatRoom
    - 채팅방에 참여한 사용자의 정보 관리 로직을 구현하기 위한 관련 레포지토리 구현
    
    - MemberChatRoomRepository.java
    - existsByChatRoomIdAndMemberUsername(), existsByChatRoomIdAndMemberNickname() 메서드 명시적으로 선언
    
    - MemberChatRoomRepositoryCustom.java
    - existsByGatherArticleIdAndNickname(), findByGatherArticleIdAndUsername() 커스텀 메서드 선언
    
    - MemberChatRoomRepositoryCustomImpl.java
    - existsByGatherArticleIdAndNickname(), findByGatherArticleIdAndUsername() 구현
    
    - 사용자의 채팅방 입장을 위한 MemberChatRoomResponse DTO 구현
    
    - MemberChatRoomResponse.ValidateDTO.java
    - 채팅방에 참여한 사용자의 정보를 조회하기 위한 DTO
    - 필드: id, memberChatRoomRole

---
+++ src/main/java/sumcoda/boardbuddy/repository/memberChatRoom/MemberChatRoomRepositoryCustomImpl.java
@@ -0,0 +1,62 @@
+package sumcoda.boardbuddy.repository.memberChatRoom;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import sumcoda.boardbuddy.dto.MemberChatRoomResponse;
+
+import java.util.Optional;
+
+import static sumcoda.boardbuddy.entity.QChatRoom.chatRoom;
+import static sumcoda.boardbuddy.entity.QGatherArticle.gatherArticle;
+import static sumcoda.boardbuddy.entity.QMember.member;
+import static sumcoda.boardbuddy.entity.QMemberChatRoom.*;
+
+@RequiredArgsConstructor
+public class MemberChatRoomRepositoryCustomImpl implements MemberChatRoomRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    /**
+     * 특정 모집글 Id와 닉네임을 가진 사용자가 존재하는지 확인
+     *
+     * @param gatherArticleId 모집글 Id
+     * @param nickname 사용자 닉네임
+     * @return 사용자가 존재하면 true, 아니면 false
+     **/
+    @Override
+    public Boolean existsByGatherArticleIdAndNickname(Long gatherArticleId, String nickname) {
+        return jpaQueryFactory
+                .selectOne()
+                .from(memberChatRoom)
+                .leftJoin(memberChatRoom.member, member)
+                .leftJoin(memberChatRoom.chatRoom, chatRoom)
+                .leftJoin(chatRoom.gatherArticle, gatherArticle)
+                .where(gatherArticle.id.eq(gatherArticleId)
+                        .and(member.nickname.eq(nickname)))
+                .fetchOne() != null;
+    }
+
+    /**
+     * 특정 모집글 Id와 사용자 아이디로 MemberChatRoom 정보를 조회
+     *
+     * @param gatherArticleId 모집글 Id
+     * @param username 사용자 아이디
+     * @return MemberChatRoom 정보가 포함된 ValidateDTO 객체
+     **/
+    @Override
+    public Optional<MemberChatRoomResponse.ValidateDTO> findByGatherArticleIdAndUsername(Long gatherArticleId, String username) {
+        return Optional.ofNullable(jpaQueryFactory
+                .select(Projections.fields(MemberChatRoomResponse.ValidateDTO.class,
+                        memberChatRoom.id,
+                        memberChatRoom.memberChatRoomRole
+                ))
+                .from(memberChatRoom)
+                .leftJoin(memberChatRoom.chatRoom, chatRoom)
+                .leftJoin(memberChatRoom.member, member)
+                .leftJoin(chatRoom.gatherArticle, gatherArticle)
+                .where(gatherArticle.id.eq(gatherArticleId)
+                        .and(member.username.eq(username)))
+                .fetchOne());
+    }
+}

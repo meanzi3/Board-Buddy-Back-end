8b7ed1ab45cc256b02ae95b09ad1b15633ed6851
geunhokinn <gghh1220@gmail.com>
2024-07-31 18:04:31 +0900

    Feat: RedisConfig 작성
    - RedisConfig.java
    - 커스텀한 redisTemplate 을 사용하고 빈으로 등록하기 위해 redisTemplate() 작성

--- src/main/java/sumcoda/boardbuddy/config/RedisConfig.java
+++ src/main/java/sumcoda/boardbuddy/config/RedisConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -34,5 +36,21 @@ public class RedisConfig {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
         return stringRedisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        // RedisTemplate 객체 생성
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // redisConnectionFactory() 를 RedisTemplate 에 설정하므로 Redis 서버와 연결할 수 있음
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        // 키, 해시 키, 해시 값을 모두 문자열로 직렬화 및 역직렬하기 위해 시리얼라이저 설정
+        // RedisTemplate 의 키 시리얼라이저 설정
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // RedisTemplate 의 해시 키 시리얼라이저 설정
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // RedisTemplate 의 해시 값 시리얼라이저 설정
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 }

8df6ceff11d8613b39182c5ffe55535cf267b80b
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-31 16:15:58 +0900

    Feat: 채팅 메세지 발행 및 전송, 채팅방 메세지 조회 요청을 캐치하기 위한 컨트롤러 구현
    - ChatMessage
    - 채팅방 입장/퇴장 요청을 캐치하기 위한 ChatMessageController.java 구현
    
    - ChatMessageController.java
    - 필드: chatMessageService
    
    - publishMessage() 메서드: 특정 채팅방에 메세지 발행 및 전송 요청 캐치
    - 요청 URI: '/api/chat/publication/{chatRoomId}'
    
    - getChatMessages() 메서드: 채팅방 메세지 내역 조회 요청 캐치
    - 요청 URI: '/api/chat/rooms/{chatRoomId}/messages'

---
+++ src/main/java/sumcoda/boardbuddy/controller/ChatMessageController.java
@@ -0,0 +1,61 @@
+package sumcoda.boardbuddy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import sumcoda.boardbuddy.dto.ChatMessageRequest;
+import sumcoda.boardbuddy.dto.ChatMessageResponse;
+import sumcoda.boardbuddy.dto.common.ApiResponse;
+import sumcoda.boardbuddy.service.ChatMessageService;
+
+import java.util.List;
+import java.util.Map;
+
+import static sumcoda.boardbuddy.builder.ResponseBuilder.buildSuccessResponseWithPairKeyData;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    private final ChatMessageService chatMessageService;
+
+    /**
+     * 특정 채팅방에 메세지 발행 및 전송
+     *
+     * @param chatRoomId 채팅방 Id
+     * @param publishDTO 발행할 메세지 내용 DTO
+     * @param username 메시지를 발행하는 사용자 이름
+     **/
+    @MessageMapping("/publication/{chatRoomId}")
+    public void publishMessage(
+            @DestinationVariable Long chatRoomId,
+            @Payload ChatMessageRequest.PublishDTO publishDTO,
+            @RequestAttribute String username) {
+
+        chatMessageService.publishMessage(chatRoomId, publishDTO, username);
+    }
+
+    /**
+     * 채팅방 메세지 내역 조회
+     *
+     * @param chatRoomId 채팅방 Id
+     * @param username 요청을 보낸 사용자 아이디
+     * @return 채팅방 메세지 내역
+     */
+    @GetMapping("/api/chat/rooms/{chatRoomId}/messages")
+    public ResponseEntity<ApiResponse<Map<String, List<ChatMessageResponse.ChatMessageInfoDTO>>>> getChatMessages(
+            @PathVariable Long chatRoomId,
+            @RequestAttribute String username) {
+
+        List<ChatMessageResponse.ChatMessageInfoDTO> chatMessages = chatMessageService.findMessagesAfterMemberJoinedByChatRoomIdAndUsername(chatRoomId, username);
+
+        return buildSuccessResponseWithPairKeyData("chatMessages", chatMessages, "채팅 메세지들의 정보를 성공적으로 조회했습니다.", HttpStatus.OK);
+    }
+}

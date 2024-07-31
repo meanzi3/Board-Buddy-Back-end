d0dee9a1d59c90a8ab02dbd35f7c661c9ba5758e
runtime-zer0 <bigstring.zero@gmail.com>
2024-07-31 15:44:52 +0900

    Feat: 채팅방 입장, 퇴장 관련 로직을 구현하기 위한 비즈니스 로직 구현
    - ChatRoom
    - 채팅방 입장, 퇴장 관련 로직을 구현하기 위한 ChatRoomService.java 구현
    
    - ChatRoomService.java
    - 필드: chatRoomRepository, memberChatRoomRepository, memberRepository
    - enterChatRoom() 메서드: 사용자가 특정 모집글과 관련된 채팅방에 입장
    - leaveChatRoom() 메서드: 사용자가 특정 모집글과 관련된 채팅방에서 퇴장
    - getChatRoomDetailsListByUsername() 메서드: 특정 사용자가 참여하고 있는 채팅방 상세 정보 목록 조회

---
+++ src/main/java/sumcoda/boardbuddy/service/ChatRoomService.java
@@ -0,0 +1,122 @@
+package sumcoda.boardbuddy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sumcoda.boardbuddy.dto.ChatRoomResponse;
+import sumcoda.boardbuddy.dto.MemberChatRoomResponse;
+import sumcoda.boardbuddy.entity.ChatRoom;
+import sumcoda.boardbuddy.entity.Member;
+import sumcoda.boardbuddy.entity.MemberChatRoom;
+import sumcoda.boardbuddy.enumerate.MemberChatRoomRole;
+import sumcoda.boardbuddy.exception.*;
+import sumcoda.boardbuddy.exception.member.MemberNotFoundException;
+import sumcoda.boardbuddy.exception.member.MemberRetrievalException;
+import sumcoda.boardbuddy.repository.chatRoom.ChatRoomRepository;
+import sumcoda.boardbuddy.repository.memberChatRoom.MemberChatRoomRepository;
+import sumcoda.boardbuddy.repository.MemberRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    private final MemberChatRoomRepository memberChatRoomRepository;
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 사용자가 특정 모집글과 관련된 채팅방에 입장
+     *
+     * @param gatherArticleId 모집글 Id
+     * @param nickname 사용자 닉네임
+     **/
+    @Transactional
+    public Long enterChatRoom(Long gatherArticleId, String nickname) {
+        ChatRoom chatRoom = chatRoomRepository.findByGatherArticleId(gatherArticleId)
+                .orElseThrow(() -> new ChatRoomNotFoundException("해당 모집글에 대한 채팅방이 존재하지 않습니다."));
+
+        Long chatRoomId = chatRoom.getId();
+
+        if (chatRoomId == null) {
+            throw new ChatRoomRetrievalException("서버 문제로 해당 채팅방의 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
+
+        Member member = memberRepository.findByNickname(nickname)
+                .orElseThrow(() -> new MemberNotFoundException("해당 사용자를 찾을 수 없습니다."));
+
+        Boolean isMemberChatRoomExists = memberChatRoomRepository.existsByGatherArticleIdAndNickname(gatherArticleId, nickname);
+        if (isMemberChatRoomExists) {
+            throw new AlreadyEnteredChatRoomException("해당 채팅방은 이미 입장한 채팅방입니다.");
+        }
+
+        MemberChatRoom memberChatRoom = MemberChatRoom.buildMemberChatRoom(LocalDateTime.now(), MemberChatRoomRole.PARTICIPANT, member, chatRoom);
+
+        Long memberChatRoomId = memberChatRoomRepository.save(memberChatRoom).getId();
+
+        if (memberChatRoomId == null) {
+            throw new MemberChatRoomSaveException("서버 문제로 채팅방 관련 사용자의 정보를 저장하지 못했습니다. 관리자에게 문의하세요.");
+        }
+
+        return chatRoomId;
+    }
+
+    /**
+     * 사용자가 특정 모집글과 관련된 채팅방에서 퇴장
+     *
+     * @param gatherArticleId 모집글 Id
+     * @param username 사용자 아이디
+     **/
+    @Transactional
+    public Long leaveChatRoom(Long gatherArticleId, String username) {
+        ChatRoomResponse.ValidateDTO chatRoomValidateDTO = chatRoomRepository.findValidateDTOByGatherArticleId(gatherArticleId)
+                .orElseThrow(() -> new ChatRoomNotFoundException("해당 모집글에 대한 채팅방이 존재하지 않습니다"));
+
+        Long chatRoomId = chatRoomValidateDTO.getId();
+        if (chatRoomId == null) {
+            throw new ChatRoomRetrievalException("서버문제로 해당 모집글에 대한 채팅방 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
+
+        MemberChatRoomResponse.ValidateDTO memberChatRoomValidateDTO = memberChatRoomRepository.findByGatherArticleIdAndUsername(gatherArticleId, username)
+                .orElseThrow(() -> new MemberChatRoomNotFoundException("채팅방 관련 사용자의 정보를 찾을 수 없습니다."));
+
+        MemberChatRoomRole memberChatRoomRole = memberChatRoomValidateDTO.getMemberChatRoomRole();
+        if (memberChatRoomRole == null) {
+            throw new MemberChatRoomRetrievalException("서버 문제로 채팅방 관련 사용자의 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
+
+        if (memberChatRoomValidateDTO.getMemberChatRoomRole() == MemberChatRoomRole.HOST) {
+            throw new ChatRoomHostCannotLeaveException("채팅방의 방장은 채팅방을 퇴장할 수 없습니다.");
+        }
+
+        Long memberChatRoomId = memberChatRoomValidateDTO.getId();
+
+        if (memberChatRoomId == null) {
+            throw new MemberChatRoomRetrievalException("서버 문제로 채팅방 관련 사용자의 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
+
+        memberChatRoomRepository.deleteById(memberChatRoomId);
+
+        return chatRoomId;
+    }
+
+    /**
+     * 특정 사용자가 참여하고 있는 채팅방 상세 정보 목록 조회
+     *
+     * @param username 사용자 아이디
+     * @return 사용자가 참여하고 있는 채팅방 상세 정보 목록
+     **/
+    public List<ChatRoomResponse.ChatRoomDetailsDTO> getChatRoomDetailsListByUsername(String username) {
+        Boolean isMemberExists = memberRepository.existsByUsername(username);
+        if (!isMemberExists) {
+            throw new MemberRetrievalException("서버 문제로 사용자의 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
+
+        return chatRoomRepository.findChatRoomDetailsListByUsername(username);
+    }
+}

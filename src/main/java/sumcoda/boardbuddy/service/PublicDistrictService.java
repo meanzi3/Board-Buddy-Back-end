b42d7ba8f11d5366b3650e56474d66c581b2173d
geunhokinn <gghh1220@gmail.com>
2024-08-01 18:18:11 +0900

    Feat: PublicDistrictController, PublicDistrictService 작성
    - PublicDistrictController.java
    - 회원가입 요청에 대한 위치 검색 요청을 처리하기 위한 메서드 이름을 searchAuthLocations() 로 수정
    - 로그인 이후 요청에 대한 위치 검색 요청을 처리하기 위해 searchLocations() 작성
    
    - PublicDistrictService.java
    - 컨트롤러의 회원가입 요청에 대한 위치 검색 요청을 처리하기 위한 메서드 이름을 searchAuthLocations() 로 수정
    - 컨트롤러의 로그인 이후 위치 검색하는 요청에 대한 로직을 처리하기 위해 searchLocations() 작성

--- src/main/java/sumcoda/boardbuddy/service/PublicDistrictService.java
+++ src/main/java/sumcoda/boardbuddy/service/PublicDistrictService.java
@@ -8,6 +8,7 @@ import sumcoda.boardbuddy.dto.PublicDistrictResponse;
 import sumcoda.boardbuddy.exception.member.MemberRetrievalException;
 import sumcoda.boardbuddy.exception.publicDistrict.NoSearchResultException;
 import sumcoda.boardbuddy.exception.publicDistrict.SearchLengthException;
+import sumcoda.boardbuddy.repository.MemberRepository;
 import sumcoda.boardbuddy.repository.publicDistrict.PublicDistrictRepository;
 
 import java.util.List;
@@ -23,20 +24,56 @@ public class PublicDistrictService {
 
     private final PublicDistrictRepository publicDistrictRepository;
 
+    private final MemberRepository memberRepository;
+
     private final PublicDistrictRedisService publicDistrictRedisService;
 
     /**
-     * 위치 검색
+     * 위치 검색 (회원 가입)
      *
      * @param emd 읍면동 검색어
      * @return 검색된 위치 정보 리스트
      */
-    public List<PublicDistrictResponse.InfoDTO> searchLocations(String emd) {
+    public List<PublicDistrictResponse.InfoDTO> searchAuthLocations(String emd) {
 
-//        // 유저 검증
-//        if (username == null) {
-//            throw new MemberRetrievalException("위치 검색 요청을 처리할 수 없습니다. 관리자에게 문의하세요.");
-//        }
+        // 검색어 길이 검증
+        if (emd.length() < MINIMUM_SEARCH_LENGTH) {
+            throw new SearchLengthException("검색어는 두 글자 이상이어야 합니다.");
+        }
+
+        // redis 에서 조회 - 위치 검색
+        List<PublicDistrictResponse.InfoDTO> infoDTOs = publicDistrictRedisService.findInfoDTOsByEmd(emd);
+
+        // mariadb 에서 조회 - 위치 검색(redis 장애 발생 시 mariadb 에서 조회)
+        if (infoDTOs != null) {
+            log.info("[redis findInfoDTOsByEmd() success]");
+        } else {
+            log.error("[redis findInfoDTOsByEmd() error]");
+            infoDTOs = publicDistrictRepository.findInfoDTOsByEmd(emd);
+        }
+
+        // 검색 결과가 없는 경우 예외 처리
+        if (infoDTOs.isEmpty()) {
+            throw new NoSearchResultException("검색 결과가 없습니다. 검색어를 다시 확인해주세요.");
+        }
+
+        // 검색된 위치 정보 리스트 반환
+        return infoDTOs;
+    }
+
+    /**
+     * 위치 검색 (로그인 이후)
+     *
+     * @param emd 읍면동 검색어
+     * @return 검색된 위치 정보 리스트
+     */
+    public List<PublicDistrictResponse.InfoDTO> searchLocations(String emd, String username) {
+
+        // 멤버 검증
+        Boolean isMemberExists = memberRepository.existsByUsername(username);
+        if (!isMemberExists) {
+            throw new MemberRetrievalException("서버 문제로 사용자의 정보를 찾을 수 없습니다. 관리자에게 문의하세요.");
+        }
 
         // 검색어 길이 검증
         if (emd.length() < MINIMUM_SEARCH_LENGTH) {

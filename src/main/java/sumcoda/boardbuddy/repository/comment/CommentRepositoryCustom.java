1bc250c07b8e3b48bad5655dd7f40f1042c04b9a
geunhokinn <gghh1220@gmail.com>
2024-08-02 19:24:54 +0900

    Feat: CommentRepositoryCustom, CommentRepositoryCustomImpl 작성
    - CommentRepositoryCustom.java, CommentRepositoryCustomImpl.java
    - 댓글 삭제 시 N+1 문제를 해결하기 위해 댓글을 가져올 때 자식 댓글까지 함께 가지고 오는 쿼리 작성

--- src/main/java/sumcoda/boardbuddy/repository/comment/CommentRepositoryCustom.java
+++ src/main/java/sumcoda/boardbuddy/repository/comment/CommentRepositoryCustom.java
@@ -1,14 +1,18 @@
 package sumcoda.boardbuddy.repository.comment;
 
 import sumcoda.boardbuddy.dto.CommentResponse;
+import sumcoda.boardbuddy.entity.Comment;
 import sumcoda.boardbuddy.entity.Member;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepositoryCustom {
 
   long countCommentsByMember(Member member, LocalDateTime startOfLastMonth, LocalDateTime endOfLastMonth);
 
-  List<CommentResponse.CommentDTO> findCommentDTOsByGatherArticleId(Long gatherArticleId);
+  List<CommentResponse.InfoDTO> findCommentDTOsByGatherArticleId(Long gatherArticleId);
+
+  Optional<Comment> findCommentByCommentId(Long commentId);
 }

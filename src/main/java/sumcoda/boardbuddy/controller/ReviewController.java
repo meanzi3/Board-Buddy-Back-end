1e97c694735ab790e59126a5a4fb449b31808df5
Dasom Lee <dsy20320@gmail.com>
2024-08-07 03:24:05 -0600

    feat: 모집글에 참가한 다른 유저 조회 기능을 위한 컨트롤러 수정 - ReviewController.java - 모집글에 참가한 다른 유저 리스트를 반환하는 getParticipatedList() 추가

--- src/main/java/sumcoda/boardbuddy/controller/ReviewController.java
+++ src/main/java/sumcoda/boardbuddy/controller/ReviewController.java
@@ -5,15 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import sumcoda.boardbuddy.dto.MemberRequest;
+import org.springframework.web.bind.annotation.*;
+import sumcoda.boardbuddy.dto.ReviewRequest;
+import sumcoda.boardbuddy.dto.ReviewResponse;
 import sumcoda.boardbuddy.dto.common.ApiResponse;
 import sumcoda.boardbuddy.service.ReviewService;
 
-import static sumcoda.boardbuddy.builder.ResponseBuilder.buildSuccessResponseWithoutData;
+import java.util.List;
+import java.util.Map;
+
+import static sumcoda.boardbuddy.builder.ResponseBuilder.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +22,24 @@ import static sumcoda.boardbuddy.builder.ResponseBuilder.buildSuccessResponseWit
 public class ReviewController {
 
     private final ReviewService reviewService;
+
+    /**
+     * 리뷰 전송 가능한 유저 리스트 조회 요청 캐치
+     *
+     * @param gatherArticleId 모집글 Id
+     * @param username        로그인 사용자 아이디
+     * @return 유저 리스트 조회가 성공했다면 약속된 SuccessResponse 반환
+     **/
+    @GetMapping("/api/reviews/{gatherArticleId}")
+    public ResponseEntity<ApiResponse<Map<String, List<ReviewResponse.ReviewDTO>>>> getParticipatedList (
+            @PathVariable Long gatherArticleId,
+            @RequestAttribute String username) {
+        log.info("get participated list is working");
+
+        List<ReviewResponse.ReviewDTO> participatedList = reviewService.getParticipatedList(gatherArticleId, username);
+
+        return buildSuccessResponseWithPairKeyData("users", participatedList, "참가한 유저 리스트 조회에 성공하였습니다.", HttpStatus.OK);
+    }
 
     /**
      * 리뷰 보내기 요청 캐치
@@ -33,7 +52,7 @@ public class ReviewController {
     @PostMapping("/api/reviews/{gatherArticleId}")
     public ResponseEntity<ApiResponse<Void>> sendReview (
             @PathVariable Long gatherArticleId,
-            @RequestBody MemberRequest.ReviewDTO reviewDTO,
+            @RequestBody ReviewRequest.ReviewDTO reviewDTO,
             @RequestAttribute String username) {
         log.info("send Review is working");
 

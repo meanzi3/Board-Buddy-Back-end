381f549ccb5f64c7b3bad04ec107b8410e26df45
Minji <minjichoe0326@gmail.com>
2024-08-09 18:34:31 +0900

    Fix: 배포환경 테스트를 위한 수정
    - Sl4fj 사용을 위한 import 추가

--- src/main/java/sumcoda/boardbuddy/service/BadgeImageService.java
+++ src/main/java/sumcoda/boardbuddy/service/BadgeImageService.java
@@ -1,6 +1,7 @@
 package sumcoda.boardbuddy.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

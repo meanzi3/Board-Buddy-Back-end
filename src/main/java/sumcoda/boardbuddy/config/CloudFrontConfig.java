8bd5316559001a0a304e9cbbeba147087c491dac
runtime-zer0 <bigstring.zero@gmail.com>
2025-07-31 23:40:03 +0900

    Refactor: AWS Secret Manger 활성화
    - Private Key를 서버에서 활용하기 위한 AWS Secret Manager 서비스 활성화 작업 및 기존 로직 수정
    
    - application-prod.yml
      - 환경 변수 수정
    
    - build.gradle
      - AWS Secret Manager 서비스 활성화를 위한 의존성 추가
    
    - CloudFrontConfig.java
      - AWS Secret Manager 서비스 활성화를 위한 프로퍼티 추가
      - secretsManagerClient 빈 메서드 추가
      - cloudFrontRSAPrivateKey() 빈 메서드 로직 수정
    
    - CloudFrontSignedUrlService.java
      - generateSignedUrl() 메서드 내에 필드 이름 수정
    
    - CloudFrontSignedUrlUtil.java
      - calculateExpiration() 메서드 로직 최적화
    
    - deploy.yml
      - 배포환경 서버에서 활용할 환경 변수 수정
      - 배포 파이프라인 동작시 필요한 올바른 환경변수 매핑

--- src/main/java/sumcoda/boardbuddy/config/CloudFrontConfig.java
+++ src/main/java/sumcoda/boardbuddy/config/CloudFrontConfig.java
@@ -4,26 +4,33 @@ import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudfront.CloudFrontUtilities;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "spring.cloud.aws.cloud-front")
-@Getter
 public class CloudFrontConfig {
 
     private String domain;
 
     private String keyPairId;
 
-    // PEM 문자열 직접 주입
-    private String privateKeyPem;
-
     private int urlExpirationMinutes;
+
+    private String secretRegion;
+
+    //Secrets Manager 시크릿 이름 프로퍼티
+    private String secretName;
+
+
 
     @Bean
     public CloudFrontUtilities cloudFrontUtilities() {
@@ -31,18 +38,31 @@ public class CloudFrontConfig {
     }
 
     @Bean
-    public RSAPrivateKey cloudFrontRSAPrivateKey() throws Exception {
-        // PEM 헤더/스페이스 제거 후 Base64 디코드
+    public SecretsManagerClient secretsManagerClient() {
+        return SecretsManagerClient.builder()
+                .region(Region.of(this.secretRegion)).build();
+    }
+
+    @Bean
+    public RSAPrivateKey cloudFrontRSAPrivateKey(SecretsManagerClient secretsManagerClient) throws Exception {
+        // 1) Secrets Manager에서 PEM 문자열 읽기
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(this.secretName)
+                .build();
+
+        String privateKeyPem = secretsManagerClient.getSecretValue(getSecretValueRequest).secretString();
+
+        // 2) PEM 헤더/스페이스 제거 후 Base64 디코드
         String base64 = privateKeyPem
                 .replace("-----BEGIN (.*)-----", "")
                 .replace("-----END (.*)-----", "")
                 .replaceAll("\\s+", "");
 
-        byte[] der = Base64.getDecoder().decode(base64);
+        byte[] decode = Base64.getDecoder().decode(base64);
 
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(der);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decode);
 
-        return (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(spec);
+        return (RSAPrivateKey) KeyFactory.getInstance("RSA")
+                .generatePrivate(keySpec);
     }
-
 }
